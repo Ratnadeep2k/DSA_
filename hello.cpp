b031cfd410483c298d6b47fc1cee86e4b048429c
ratnadeep2k <ratnadeepboruah365@gmail.com>
2024-03-28 12:47:31 +0530

    modified

--- hello.cpp
+++ hello.cpp
@@ -9,11 +9,5 @@ int main(){
     for(size_t i=0;i<n;i++){
         cin>>arr[i];
     }
-    for (size_t i = 0; i <n; i++)
-    {
-        /* code */
-        cout<<arr[i]<<" "<<endl;
-    }
-    return 0;
     
 }
