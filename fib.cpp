9a6e8e844f28041c6fd62b368e371375648c34c3
ratnadeep2k <ratnadeepboruah365@gmail.com>
2024-03-31 00:36:33 +0530

    DpFib

---
+++ fib.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fun(int n ,vector<int> &dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=fun(n-1,dp)+fun(n-2,dp);
+        
+}
+
+int main()
+{
+        /*
+        *  Write your code here. 
+         *  Read input as specified in the question.
+         *  Print output as specified in the question.
+        */
+        int n;
+        cin>>n;
+        vector<int> dp(n+1,-1);
+        cout<<fun(n,dp);
+        return 0;
+}
