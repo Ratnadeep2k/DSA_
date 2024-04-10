0e3f13fca7f318b3151755af42591a8622da692c
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-04-10 22:15:07 +0530

    QuickSort

---
+++ QuickSort.cpp
@@ -0,0 +1,38 @@
+
+int partition(vector<int> &arr,int low,int high){
+    int pivot = arr[low];
+    int i =low;
+    int j = high;
+    while( i <j){
+        while(arr[i]<=pivot && i<=high-1){
+            i++;
+        }
+        while(arr[j]> pivot && j>=low-1){
+            j--;
+        }
+        if (i<j)
+        {
+            swap(arr[i],arr[j]);
+        }
+
+        
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+
+
+
+
+void qs(vecttor<int> &arr,int low ,int high){
+    if(low<high){
+        int pi = partition(arr,low,high);
+        qs(arr,low,pi-1);
+        qs(arr,pi+1,high);
+    }
+}
+vector<int> quickSort(vector<int> arr)
+{
+    qs(arr,0,arr.size()-1);
+    return arr;
+}
