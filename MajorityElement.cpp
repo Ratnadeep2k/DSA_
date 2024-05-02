8be2f5954da5da952208b1922698ca844a76a3a4
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-05-02 23:20:46 +0530

    change the list

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,16 +1,16 @@
 vector<int> majorityElement(vector<int> v) {
 	// Write your code here
-	vector<int> ls;
+	vector<int> ans;
 	map<int,int> mpp;
 	int mini = (int)(v.size()/3) +1;
 	for(int i =0;i<v.size();i++){
 		mpp[v[i]]++;
 		if(mpp[v[i]]==mini) {
-			ls.push_back(v[i]);
+			ans.push_back(v[i]);
 		}
-		if(ls.size()==2) break;
+		if(ans.size()==2) break;
 
 	}
-	sort(ls.begin(),ls.end());
-	return ls;
+	sort(ans.begin(),ans.end());
+	return ans;
 }
