7798bd1732800218e107a97094886c8c78a30962
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-11-01 13:32:46 +0530

    feat: Implement Bubble Sort as a Solution class with vector support

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,46 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-
-void bubbleSort(int arr[],int n){
-    for (size_t i = n-1; i>=0; i--)
-    {
-        /* code */
-        int didSwap = 0;
-        for (size_t j = 0; j<= i-1; j++)
-        {
-            /* code */
-            if(arr[j]>arr[j+1]){
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-                didSwap = 1;
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        for(size_t i =0;i<n-1;i++){
+            for(size_t j =0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]) swap(nums[j],nums[j+1]);
             }
-        }
-        if(didSwap == 0){
-            break;
-        }
-        
-    }
-    
-}
-int main(){
 
-
-    int n;
-    cin>>n;
-    int arr[n];
-    for (size_t i = 0; i < n; i++)
-    {
-        /* code */
-        cin>>arr[i];
+        }
+        return nums;
     }
-    bubbleSort(arr,n);
-    for (size_t i = 0; i < n; i++)
-    {
-        /* code */
-        cout<<arr[i]<<" ";
-    }
-    return 0;
-}
+};
 
